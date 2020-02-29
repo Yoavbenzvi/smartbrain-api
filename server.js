7bad2f9839dcd83ac3c4de2a2fdcc806d01df60f
YoavBZ <yoav.benzvi@gmail.com>
2020-02-29 14:08:26 +0100

    changing database reference

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ const db = knex({
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {res.send(database.users)});
+app.get('/', (req, res) => {res.send('app is working')});
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
-app.listen(process.env.PORT || 30001 , () => {
+app.listen(process.env.PORT || 30s01 , () => {
 	console.log(`app is  running on port ${process.env.PORT}`);
 })
